7ae590ad00a5021b007db63f0d39693f816f7ed8
Pati <patrick.deutsch@outlook.com>
2015-11-07 12:44:18 +0100

    Jackson

--- src/main/java/at/ac/uibk/UserService.java
+++ src/main/java/at/ac/uibk/UserService.java
@@ -1,22 +1,39 @@
 package at.ac.uibk;
 
+import java.util.Collection;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import data.handler.DataHandler;
+import data.model.SavedUser;
 
 @Path("/user")
-@Produces("text/html")
+//@Produces("text/html")
 public class UserService {
 
+	private DataHandler handler;
+	
+	public UserService(){
+		handler = new DataHandler();
+	}
+	
     @GET
-    public String getHello() {
-        return "Hello User!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<SavedUser> getHello() {
+        
+    	return handler.getAllUsers();
+    	
     }
 
     @GET
-    @Path("/{username}")
-    public String getHello(@PathParam("username") String username){
-        return "Hello " + username + "!";
+    @Path("/{userid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public SavedUser getHello(@PathParam("userid") int userid){
+
+    	return handler.getUserByID(userid);
     }
 }
