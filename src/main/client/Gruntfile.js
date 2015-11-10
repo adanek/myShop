9af7604f81abe848892aeb92cebace43595315a2
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-11-10 01:59:58 +0100

    Angular Bug Fixing

--- src/main/client/Gruntfile.js
+++ src/main/client/Gruntfile.js
@@ -22,8 +22,8 @@ module.exports = function (grunt) {
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
-    dist: 'dist'
-    //dist: '../webapp'
+    //dist: 'dist'
+    dist: '../webapp'
   };
 
   // Define the configuration for all the tasks
