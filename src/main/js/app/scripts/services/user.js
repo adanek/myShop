981035c69c03b7e594b618b6e6c2e3d4f0c963d1
adanek <andreas.danek-bulius@student.uibk.ac.at>
2015-11-15 21:53:00 +0100

    Fix comments

--- src/main/js/app/scripts/services/user.js
+++ src/main/js/app/scripts/services/user.js
@@ -6,27 +6,32 @@
 
     var srv = this;
     var authenticated = false;
-    var user= {alias: "Tom Riddle", id: 999, rights: {
-      canCreateItem: false
-    }};
+    var user = {
+      alias: "Tom Riddle",
+      id: 999,
+      rights: {
+        canCreateItem: false,
+        canCreateComment: true
+      }
+    };
 
     srv.isAuthenticated = function () {
       return authenticated;
     };
 
-    srv.isAdmin = function (){
+    srv.isAdmin = function () {
       return user.role === 'admin';
     };
 
-    srv.canCreateProduct = function (){
+    srv.canCreateProduct = function () {
       return user.rights.canCreateItem;
     }
 
-    srv.canCreateComment = function(){
+    srv.canCreateComment = function () {
       return user.rights.canCreateComment;
     };
 
-    srv.canEditComment= function(comment){
+    srv.canEditComment = function (comment) {
       return comment.author === user.alias || user.role === 'admin';
     };
 
@@ -42,11 +47,11 @@
       }
     }
 
-    srv.getUsername = function(){
-      return user.alias == undefined ? '': user.alias;
+    srv.getUsername = function () {
+      return user.alias == undefined ? '' : user.alias;
     }
 
-    srv.getID = function(){
+    srv.getID = function () {
       return user.id;
     }
 
@@ -54,27 +59,27 @@
 
       var hash = CryptoJS.SHA1(password).toString(CryptoJS.enc.Base64);
       return $http.post('api/users/login', {name: username, hash: hash}).then(
-        function successCallback(response){
+        function successCallback(response) {
           user = response.data;
           srv.setAuthenticated(true);
 
           $rootScope.$broadcast('user-login');
           return response;
         },
-        function errorCallback(response){
+        function errorCallback(response) {
           return response;
         });
     }
 
-    srv.logout = function(){
+    srv.logout = function () {
       return $http.post('api/users/logout').then(
-        function successCallback(response){
+        function successCallback(response) {
           srv.setAuthenticated(false);
           user = {};
           $location.path('/#').replace();
           return response;
         },
-        function errorCallback(response){
+        function errorCallback(response) {
           return $q.reject(response);
         });
     };
@@ -83,16 +88,16 @@
      * Registers a new user on the server
      * @param name the name of the user
      * @param password the password of the user in cleartext
-       * @returns Promise<User>
-       */
-    srv.register = function(name, password){
+     * @returns Promise<User>
+     */
+    srv.register = function (name, password) {
       var hash = CryptoJS.SHA1(password).toString(CryptoJS.enc.Base64);
 
       return $http.post('api/users/register', {name: name, hash: hash}).then(
-        function successCallback(response){
+        function successCallback(response) {
           return response;
         },
-        function errorCallback(response){
+        function errorCallback(response) {
           console.log("Something went wrong");
           return $q.reject(response);
         }
