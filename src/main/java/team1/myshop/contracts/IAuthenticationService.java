4eb758dcc66aa6d24b730af0c6b916c22f8376b5
Pati <patrick.deutsch@outlook.com>
2015-12-01 14:03:10 +0100

    check_own_user

--- src/main/java/team1/myshop/contracts/IAuthenticationService.java
+++ src/main/java/team1/myshop/contracts/IAuthenticationService.java
@@ -3,10 +3,14 @@ package team1.myshop.contracts;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import team1.myshop.web.model.UserInfo;
+
 public interface IAuthenticationService {
 	boolean checkGetUserInfo(HttpServletRequest request, HttpServletResponse response, int userid);
 
 	void ensureUserRight(HttpServletRequest request, HttpServletResponse response, UserRights right);
 
 	boolean userHasRight(HttpServletRequest request, UserRights right);
+
+	UserInfo getUserInfo(HttpServletRequest request);
 }
