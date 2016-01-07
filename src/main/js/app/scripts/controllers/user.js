b26e00e6123145e9b0162a3d8139740cc1bd96b3
adanek <andreas.danek-bulius@student.uibk.ac.at>
2016-01-07 12:01:27 +0100

    shops

--- src/main/js/app/scripts/controllers/user.js
+++ src/main/js/app/scripts/controllers/user.js
@@ -3,44 +3,51 @@
 (function (app) {
   app.controller('UserCtrl', ['$scope', 'User', function ($scope, User) {
 
-    $scope.name = User.getUsername();
     $scope.user = User.getUserInfo();
-    $scope.defaults = {
-      scrollWheelZoom: false
-    };
 
-    $scope.center = {
-      lat: 47.269203,
-      lng: 11.402229,
-      zoom: 12
-    };
-
-    $scope.layers = {
-      baselayers: {
-        googleTerrain: {
-          name: 'Google Terrain',
-          layerType: 'TERRAIN',
-          type: 'google'
-        },
-        googleHybrid: {
-          name: 'Google Hybrid',
-          layerType: 'HYBRID',
-          type: 'google'
-        },
-        googleRoadmap: {
-          name: 'Google Streets',
-          layerType: 'ROADMAP',
-          type: 'google'
+    $scope.map = {
+      defaults: {
+        scrollWheelZoom: false
+      },
+      center: {
+        lat: 47.269203,
+        lng: 11.402229,
+        zoom: 10
+      },
+      layers: {
+        baselayers: {
+          googleTerrain: {
+            name: 'Google Terrain',
+            layerType: 'TERRAIN',
+            type: 'google'
+          },
+          googleHybrid: {
+            name: 'Google Hybrid',
+            layerType: 'HYBRID',
+            type: 'google'
+          },
+          googleRoadmap: {
+            name: 'Google Streets',
+            layerType: 'ROADMAP',
+            type: 'google'
+          }
         }
-      }
-    };
+      },
+      markers : []
+    }
 
-    var home = User.getUserInfo().address.marker;
-    console.log(home);
-    $scope.markers = {
-      home: home
-    };
+    // Zoom to home
+    if($scope.user.address && $scope.user.address.marker){
+      var home = $scope.user.address.marker;
 
+      $scope.map.markers = {
+        home: home
+      };
+
+      $scope.map.center.lat = home.lat;
+      $scope.map.center.lng = home.lng;
+      $scope.map.center.zoom = 12;
+    }
   }]);
 })(angular.module('myshopApp'));
 
